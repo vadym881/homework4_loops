4d143649d19451f37e2523027d6e6170d4410655
Vadym <vadimiaroshenko@gmail.com>
2024-06-02 13:35:21 +0300

    task2 updated

--- hw_4.2.py
+++ hw_4.2.py
@@ -7,7 +7,7 @@ list1 = [0, 1, 7, 2, 4, 8]
 if not list1:
     result = 0
 else:
-    sum_even_index = sum(list1[el] for el in range(0, len(list1), 2))
+    sum_even_index = sum(list1[index] for index in range(0, len(list1), 2))
     result = sum_even_index * list1[-1]
 
 print(f'{list1} -> {result}')
